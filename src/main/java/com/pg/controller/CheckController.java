efb8f4cfb5e42b56c0c776402bd0b35f8c817ddc
mtouch <mtouch@DESKTOP-JIN>
2020-10-08 15:43:39 +0900

    체크수정

--- src/main/java/com/pg/controller/CheckController.java
+++ src/main/java/com/pg/controller/CheckController.java
@@ -3,7 +3,6 @@ package com.pg.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.pg.service.CheckService;
 
@@ -16,13 +15,12 @@ public class CheckController {
 	CheckService checkService;
 	
 	@RequestMapping(value="/check") 
-	public ModelAndView test() throws Exception{
-		ModelAndView mv = new ModelAndView();
-		
+	public String test() throws Exception{
+
 		String check = checkService.check();
 		
 		log.info("check:" + check);
 		
-		return mv;
+		return "OK";
 	}
 }
